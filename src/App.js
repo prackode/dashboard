95eaa262ec6c3e9791cf8745c22465530716b299
prakhar.ag <prakhar.agarwal@mckinleyrice.co>
2024-01-09 23:35:46 +0530

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <div class="main-content">Main Content</div>
+      <div class="sidebar">
+        <div className="fixed">Sidebar</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
